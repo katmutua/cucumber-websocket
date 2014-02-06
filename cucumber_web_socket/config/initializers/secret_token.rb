6b286b680d91668ef05a6dddb1a921fe0854f38f
katmutua <jkm.mutua@gmail.com>
2014-02-05 23:22:51 -0200

    [Initial commit] Add rails project

---
+++ cucumber_web_socket/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CucumberWebSocket::Application.config.secret_key_base = '28298eb50c175935461eb8df28a05e0b98cc8cd101219bee94716192675e313850bf330ca8d8a55ec689852b09a2ce4796f064785346b15cb48073ce6cf992e9'
---
+++ cucumber_web_socket/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CucumberWebSocket::Application.config.session_store :cookie_store, key: '_cucumber_web_socket_session'
